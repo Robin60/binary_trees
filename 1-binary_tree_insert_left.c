deca578308d1411afebb6384b099aa005a99a0e6
Robin Owino <owinorobin05@gmail.com>
2023-03-31 02:04:16 -0700

    fix: hot fixes

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -11,7 +11,6 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new = NULL;
 
-	new = malloc(sizeof(binary_tree_t));
 	if (parent == NULL)
 		return (NULL);
 	new = binary_tree_node(parent, value);
